369e792afad34fc8f0c03b1a73d2e47c71910d6b
PoojariHemanth <hemanth2795@gmail.com>
2022-06-03 12:49:35 +0530

    This is my first commit

---
+++ Batch23Testng/src/test/java/Utilites/FilePath.java
@@ -0,0 +1,11 @@
+package Utilites;
+
+public interface FilePath {
+
+	
+	public static String ChromePath ="C:\\Users\\Hemanth\\Downloads\\Batch23_hemanth_assessment05-main\\Batch23_hemanth_assessment05-main\\src\\test\\resources\\Drivers";
+	public static String PropertiesPath ="C:\\Users\\Hemanth\\Downloads\\Batch23_hemanth_assessment05-main\\Batch23_hemanth_assessment05-main\\src\\test\\resources\\ConfigFiles";
+	public static String XLPath ="C:\\Users\\Hemanth\\Downloads\\Batch23_hemanth_assessment05-main\\Batch23_hemanth_assessment05-main\\src\\test\\resources\\ExcelFiles";
+	
+
+}
